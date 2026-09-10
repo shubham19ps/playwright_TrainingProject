8b34c4b9863b120610b948021132c1bde02d0ee4
shubham19ps <shubham@publicissapient.com>
2026-09-10 16:57:09 +0530

    Set headless mode to true in Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
     video: 'on',
     screenshot: 'on',
     testIdAttribute: 'data-test',
-    headless: false,
+    headless: true,
   },
 
   /* Configure projects for major browsers */
